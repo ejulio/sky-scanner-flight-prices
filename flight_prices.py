e1a443e35857f343f8e453e788f88887da6de041
Denis <dlcgermano@gmail.com>
2019-08-20 11:37:58 -0300

    Add support to several countries on origin

--- flight_prices.py
+++ flight_prices.py
@@ -1,8 +1,8 @@
-from argparse import ArgumentParser
 import time
+from argparse import ArgumentParser
+from itertools import product
 
 import pandas as pd
-
 import sky_scanner_rapid_api
 
 
@@ -10,10 +10,10 @@ def parse_args():
     ap = ArgumentParser()
 
     ap.add_argument('--origin', type=str, required=True,
-        help='Origin airports (Sky scanner API, may require "-sky" suffix) separated by comma ",".')
+        help='Origin airports separated by comma ",".')
 
     ap.add_argument('--destination', type=str, required=True,
-        help='Destination airports (Sky scanner API, may require "-sky" suffix) separated by comma ",".')
+        help='Destination airports separated by comma ",".')
 
     ap.add_argument('--departure-date', type=str, required=True,
         help='Departure date formatted as YYYY-MM-DD.')
@@ -30,13 +30,27 @@ def parse_args():
     ap.add_argument('--output', type=str, required=True,
         help='Output CSV file.')
 
+    ap.add_argument('--output-summary', type=str, required=False,
+        help='Output CSV file.')
+
     return ap.parse_args()
 
 
-def build_query_queue(origin, destination):
-    for o in origin.split(','):
-        for d in destination.split(','):
-            yield (o.strip(), d.strip())
+def build_query_queue(origin, destination, country):
+    travels = product(origin.split(','), destination.split(','))
+    for origin, destination in travels:
+        origin_country = country
+        try:
+            temp = origin.split("/")
+            origin, origin_country = temp
+        except:
+            pass
+
+        if origin == destination:
+            print(f'Skipping same location ({origin} -> {destination}).')
+            continue
+
+        yield (f"{origin.strip()}-sky", origin_country, f"{destination.strip()}-sky")
 
 
 if __name__ == '__main__':
@@ -46,9 +60,10 @@ if __name__ == '__main__':
         columns=['origin', 'destination', 'departure_date', 'return_date', 'price', 'country', 'link']
     )
 
-    query_queue = list(build_query_queue(args.origin, args.destination))
+    query_queue = list(build_query_queue(args.origin, args.destination, args.country))
     while query_queue:
-        (origin, destination) = query_queue.pop()
+        (origin, country, destination) = query_queue.pop()
+
         try:
             time.sleep(1)
             offers = list(sky_scanner_rapid_api.get_prices_for(
@@ -56,7 +71,7 @@ if __name__ == '__main__':
                 destination,
                 args.departure_date,
                 args.return_date,
-                args.country
+                country
             ))
             if offers:
                 df = df.append(offers)
@@ -69,3 +84,10 @@ if __name__ == '__main__':
 
     print(f'Writing offers to {args.output}')
     df.to_csv(args.output)
+
+    if args.output_summary:
+        print(f'Writing summary to {args.output_summary}')
+        prices = df.groupby(by=['origin', 'destination'])['price'].nsmallest(30)
+        prices = prices.groupby(by=['origin', 'destination'])
+        prices = prices.median().unstack()
+        prices.to_csv(args.output_summary)
